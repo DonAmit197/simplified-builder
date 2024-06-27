74cf4e2cc5c2727053bbf62e975510af4641eac5
Frances Brussee <frances.brussee@datacom.co.nz>
2024-06-27 15:32:53 +1200

    FB: Remove demo load

--- src/component/my-forms/form-grid.tsx
+++ src/component/my-forms/form-grid.tsx
@@ -2,17 +2,12 @@ import CheckIcon from '@mui/icons-material/Check';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import SecurityIcon from '@mui/icons-material/Security';
 import {Box, Chip, IconButton, Stack, Typography} from '@mui/material';
-import dayjs from 'dayjs';
 import {MaterialReactTable, MRT_ColumnDef, useMaterialReactTable} from 'material-react-table';
 import {useEffect, useMemo, useState} from 'react';
-import {useNavigate} from 'react-router-dom';
 import {Form} from 'src/__generated__/graphql.ts';
 import StyledButton from 'src/component/shared/basic-controls/button/styled-button.tsx';
-import {RoutesEnum} from 'src/routes.tsx';
 import {FormService} from 'src/services/form.service.ts';
 import {formatDate} from 'src/services/format.service.ts';
-import {useFormStore} from 'src/store/form-store.ts';
-import {useReloadStore} from 'src/store/reload-store.ts';
 
 export interface IFormGridProps {
   selectedCategory: number;
@@ -21,11 +16,7 @@ export interface IFormGridProps {
 
 const FormGrid = ({selectedCategory, searchText}: IFormGridProps) => {
   const [data, setData] = useState<Form[] | null>(null);
-  const {setName, setUrl} = useFormStore();
-  const {forceReload} = useReloadStore();
-
   const formService = new FormService();
-  const navigate = useNavigate();
 
   useEffect(() => {
     if (selectedCategory === 0) {
@@ -34,20 +25,6 @@ const FormGrid = ({selectedCategory, searchText}: IFormGridProps) => {
 
     formService.getForms(selectedCategory, searchText).then((result) => setData(result));
   }, [selectedCategory, searchText]);
-
-  const handleEditClick = (title: string) => {
-    if (title === 'Prototype demo form') {
-      const date = dayjs();
-
-      forceReload(RoutesEnum.Builder);
-
-      setName(title);
-      const strippedName = title.replace(/[^a-zA-Z0-9]/g, '');
-      setUrl(`prod.formbuilder.govt.nz/${strippedName}${date.format('DDMMYYYY')}`);
-
-      navigate(`/${RoutesEnum.Builder}`.replace(':id', '1'));
-    }
-  };
 
   const columns = useMemo<MRT_ColumnDef<Form>[]>(
     () => [
@@ -99,23 +76,19 @@ const FormGrid = ({selectedCategory, searchText}: IFormGridProps) => {
       },
     },
     positionActionsColumn: 'last',
-    renderRowActions: ({row}) => (
+    renderRowActions: () => (
       <Stack direction='row-reverse' sx={{alignSelf: 'start', alignItems: 'center'}}>
         <IconButton aria-label='More options'>
           <MoreVertIcon />
         </IconButton>
         <Box sx={{borderRight: '2px solid', borderColor: 'grey.500'}}>
-          <StyledButton
-            variant='text'
-            aria-label='Edit form'
-            onClick={() => handleEditClick(row.original.formSettings.title)}>
+          <StyledButton variant='text' aria-label='Edit form'>
             Edit
           </StyledButton>
         </Box>
       </Stack>
     ),
   });
-
   return <MaterialReactTable table={table} />;
 };
 
--- src/pages/builder.tsx
+++ src/pages/builder.tsx
@@ -2,7 +2,6 @@ import Box from '@mui/material/Box';
 import {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import Builder from 'src/component/builder/Builder.tsx';
-import {demoJson} from 'src/component/builder/demo-json.ts';
 import staticComponents from 'src/component/builder/staticComponents';
 import StyledButton from 'src/component/shared/basic-controls/button/styled-button.tsx';
 import {RoutesEnum} from 'src/routes.tsx';
@@ -14,8 +13,6 @@ const BuilderPage = () => {
   const {reloadPage, stopReload} = useReloadStore();
   const {setTitle, setHasSubMenu, setBackUrl} = useThemeStore();
   const {name} = useFormStore();
-
-  const [forceDemo, setForceDemo] = useState(false);
 
   const navigate = useNavigate();
 
@@ -34,10 +31,6 @@ const BuilderPage = () => {
     setTitle(name ?? 'Unnamed form');
     setHasSubMenu(false);
     setBackUrl('');
-
-    if (name === 'Prototype demo form') {
-      setForceDemo(true);
-    }
   }, [name]);
 
   useEffect(() => {
@@ -46,13 +39,6 @@ const BuilderPage = () => {
       navigate(0);
     }
   }, [reloadPage]);
-
-  useEffect(() => {
-    if (forceDemo) {
-      handleCopy(demoJson);
-      setForceDemo(false);
-    }
-  }, [forceDemo]);
 
   return (
     <Box sx={{width: '100%'}}>
